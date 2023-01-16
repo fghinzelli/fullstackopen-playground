a3d46fba54945a8eaec93f40f09a1a184f4a79a1
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-01-16 14:50:17 -0300

    Delete resource

--- node-part3/index.js
+++ node-part3/index.js
@@ -42,7 +42,12 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id);
 
+  response.status(204).end();
+})
 
 const PORT = 3001;
 
